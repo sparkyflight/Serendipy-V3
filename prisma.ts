1ed6014122cacc881275cad98cb381d1324d7fc5
selectdev <selectdev01@gmail.com>
2024-08-25 06:23:19 +0000

    fix private accounts

--- prisma.ts
+++ prisma.ts
@@ -346,7 +346,8 @@ class Posts {
 			},
 		});
 
-		return docs.filter((p) => p.user.state != "BANNED");
+        //@ts-expect-error
+		return docs.filter((p) => p.user.state != "BANNED" || p.user.state != "PRIVATE");
 	}
 
 	static async updatePost(id: string, data: any) {
@@ -383,8 +384,9 @@ class Posts {
 				},
 			},
 		});
-
-		return docs.filter((p) => p.user.state != "BANNED");
+        
+        //@ts-expect-error
+		return docs.filter((p) => p.user.state != "BANNED" || p.user.state != "PRIVATE");
 	}
 
 	static async delete(PostID: string) {
@@ -427,12 +429,20 @@ class Posts {
 
 	static async upvote(PostID: string, UserID: string) {
 		try {
-			await prisma.upvotes.create({
+            const user = await prisma.users.findUnique({
+                where: {
+                    userid: UserID,
+                }
+            });
+
+            //@ts-expect-error
+			if (user.state != "VOTE_BANNED" || user.state != "BANNED") await prisma.upvotes.create({
 				data: {
 					postid: PostID,
 					userid: UserID,
 				},
 			});
+            else throw new Error("User cannot vote for posts. Reason: Punishment");
 
 			return true;
 		} catch (err) {
@@ -442,13 +452,21 @@ class Posts {
 
 	static async downvote(PostID: string, UserID: string) {
 		try {
-			await prisma.downvotes.create({
+			const user = await prisma.users.findUnique({
+                where: {
+                    userid: UserID,
+                }
+            });
+
+            //@ts-expect-error
+			if (user.state != "VOTE_BANNED" || user.state != "BANNED") await prisma.downvotes.create({
 				data: {
 					postid: PostID,
 					userid: UserID,
 				},
 			});
-
+            else throw new Error("User cannot vote for posts. Reason: Punishment");
+            
 			return true;
 		} catch (err) {
 			return err;
@@ -462,7 +480,13 @@ class Posts {
 		Image: string
 	) {
 		try {
-			await prisma.comments.create({
+			const user = await prisma.users.findUnique({
+                where: {
+                    userid: UserID,
+                }
+            });
+
+			if (user.state != "BANNED") await prisma.comments.create({
 				data: {
 					postid: PostID,
 					commentid: crypto.randomUUID().toString(),
@@ -471,6 +495,7 @@ class Posts {
 					image: Image,
 				},
 			});
+            else throw new Error("User cannot comment on posts. Reason: Punishment");
 
 			return true;
 		} catch (err) {
